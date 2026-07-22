8a765ba9e2db5a2e2a0cd12faace40cd689ed784
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-07-22 17:34:31 +0330

    custom user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -16,4 +16,30 @@ class User(AbstractUser):
     national_code = models.CharField(max_length=10, null=True, blank=True, unique=True, verbose_name="کد ملی")
     department = models.CharField(max_length=100, null=True, blank=True, verbose_name="بخش/دپارتمان")
     avatar = models.ImageField(upload_to="avatar/%Y/%m", null=True, blank=True, verbose_name="تصویر پروفایل")
-    
+
+    last_login_ip = models.GenericIPAddressField(null=True, blank=True, verbose_name="آخرین آدرس IP ورود")
+    is_verified = models.BooleanField(default=False, verbose_name="تاییدیه دارد؟")
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="تاریخ به روزرسانی")
+
+    @property
+    def is_admin(self):
+        return self.user_type == self.UserType.ADMIN
+
+    @property
+    def is_expert(self):
+        return self.user_type == self.UserType.EXPERT
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}".strip() or self.username
+
+    def __str__(self):
+        return f"{self.full_name}"
+
+    class Meta:
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربران"
+        ordering = ["-created_at"]
+        db_table = "accounts_user"
